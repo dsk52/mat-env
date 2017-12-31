d650f5f8126fae5cc9d37e42d133b4c3977d3852
Daisuke Konishi <skydai1151@gmail.com>
2018-01-01 03:10:26 +0900

    adjust syntax in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import babel from "rollup-plugin-babel"
-import uglify from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
 
 export default {
-    input: 'src/javascript/script.js',
-    output: {
-        file: 'dest/javascript/script.js',
-        format: 'iife',
-        name: 'main',
-    },
-    plugins: [
-        babel(),
-        uglify()
-    ],
-    global: {
-        // jquery: '$'  // When using jquery, it must be active.
-    }
-};
+  input: 'src/javascript/script.js',
+  output: {
+    file: 'dest/javascript/script.js',
+    format: 'iife',
+    name: 'main'
+  },
+  plugins: [
+    babel(),
+    uglify()
+  ],
+  global: {
+    // jquery: '$'  // When using jquery, it must be active.
+  }
+}
