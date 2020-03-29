eadbbda40f53c6746660d0f6707110fefe93db08
Daisuke KONISHI <skydai1151@gmail.com>
2020-03-29 12:29:03 +0900

    module update
    fix rollup entry path

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
 
 export default {
-  input: 'src/javascript/script.js',
+  input: './mat/src/javascript/script.js',
   output: {
     file: 'dest/javascript/script.js',
     format: 'iife',
